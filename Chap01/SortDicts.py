5f096e0e1b454b798b4979127292b49d8872705f
folger <folger@originlab.com.cn>
2014-05-25 15:21:25 +0800

    Chap02

--- Chap01/SortDicts.py
+++ Chap01/SortDicts.py
@@ -1,7 +1,7 @@
 from operator import itemgetter
 
 
-rows= [
+rows = [
     {'fname': 'Brian', 'lname': 'Jones', 'uid': 1003},
     {'fname': 'David', 'lname': 'Beazley', 'uid': 1002},
     {'fname': 'John', 'lname': 'Cleese', 'uid': 1001},
