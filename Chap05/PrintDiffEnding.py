12b24aca07f655b3593fe568547b9776e842fb3c
folger <folger@originlab.com.cn>
2014-06-06 10:51:54 +0800

    more learnings

---
+++ Chap05/PrintDiffEnding.py
@@ -0,0 +1,5 @@
+print('yes', 'no', 'I', 'love', 'you', sep='\n')
+print('yes', 'no', 'I', 'love', 'you', sep=',', end='!!!!!!\n')
+
+row = ('ACME', 50, 90.5)
+print(*row, sep=',')
---
+++ Chap05/PrintToFile.py
@@ -0,0 +1,4 @@
+with open('sometext.txt', 'w') as f:
+    print('hello world', file=f)
+    print('folger lun', file=f)
+    print('cindy qin', file=f)
